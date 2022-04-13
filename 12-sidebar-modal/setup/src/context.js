973ca071886c055c12e7d3ef00dfb168b474f775
ITHwang <intaek428@gmail.com>
2022-04-13 22:00:42 +0900

    finish 12-sidebar

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,41 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSiderbarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openSidebar = () => {
+    setIsSiderbarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSiderbarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
